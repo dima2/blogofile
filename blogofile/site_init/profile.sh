9abaf6256f5913f61f13270f27f6269cf9a67972
Dima2 <dima@dmi3.net>
2013-06-24 09:51:52 +0300

    profile

--- blogofile/site_init/profile.sh
+++ blogofile/site_init/profile.sh
@@ -3,5 +3,5 @@ blog_disqus_secret_key=""
 blog_disqus_public_key=""
 blog_disqus_forum=""
 blog_ftp_ip=""
-blog_user_password="user:password"
+blog_user_password="user:password" #ftp username & password in format user:password
 blog_cloudflare_api_key=""
