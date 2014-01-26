13f237993b12250b3f75d78ea1393fafd7dd408b
Dmitry <dima@dmi3.net>
2014-01-26 07:07:41 +0200

    Update profile.sh

--- blogofile/site_init/profile.sh
+++ blogofile/site_init/profile.sh
@@ -5,4 +5,4 @@ blog_disqus_forum=""
 blog_ftp_ip=""
 blog_user_password="user:password" #ftp username & password in format user:password
 blog_cloudflare_api_key=""
-deploy_content_as_branch=true
+deploy_content_as_branch=false
